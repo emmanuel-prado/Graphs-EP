fa6b486e41b4431ea60e8e6f33d3cac190f24c18
Emmanium <pradoe.ep@gmail.com>
2020-04-24 17:31:50 -0600

    got tests passing

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,63 +1,52 @@
-# pylint: disable=no-member
-
-# https://docs.python.org/3/library/typing.html
-# Integer Types -  int, float, complex
-# Sequence Types - list, tuple, range, strings
-# Set Types - set, frozenset
-# Mapping Types - dict (a mapping object maps hashable values to arbitrary objects)
-from typing import List, Dict, Tuple, Set
-
-AncestryList = List[Tuple(int, int)]
-VisitedSet = Set[int]
-Stack = List[int]
-
-
 class Graph:
     def __init__(self):
-        self.vertices = Dict[int, set]
+        self.vertices = {}
 
-    def build_graph(self, dataset: AncestryList) -> str:
-        visited = VisitedSet
+    def build_graph(self, dataset):
+        # x1 is child, x0 is parent, this graph only adds parents to the tree as values for quicker earliest ancestry lookup
         for x in dataset:
-            if x[0] and x[1] not in visited:
-                visited.add(x[0])
-                visited.add(x[1])
-                self.vertices[x[0]].add(x[1])
+            if x[1] not in self.vertices:
+                self.vertices[x[1]] = set()
                 self.vertices[x[1]].add(x[0])
+                if x[0] not in self.vertices:
+                    self.vertices[x[0]] = set()
+            else:
+                self.vertices[x[1]].add(x[0])
+                if x[0] not in self.vertices:
+                    self.vertices[x[0]] = set()
 
-        return f"successfully compiled graph"
-
-    def get_edges(self, dict_key: int) -> int:
+    def get_edges(self, dict_key):
         return self.vertices[dict_key]
 
 
-def earliest_ancestor(ancestors: AncestryList, starting_node: int):
+def earliest_ancestor(ancestors, starting_node):
     graph = Graph()
     graph.build_graph(ancestors)
-
-    s = Stack
-    visited = VisitedSet
+    s = []
+    visited = set()
     # keep track of level, start at 0
     level = 0
-    tree = List[tuple]
+    tree = []
 
-    s.push((starting_node, level))
+    if graph.vertices[starting_node] == set():
+        return -1
+
+    s.append((starting_node, level))
     while len(s) > 0:
         v = s.pop()
 
         if v[0] not in visited:
-            level += 1
             visited.add(v[0])
             tree.append((v[0], level))
-
             for edge in graph.vertices[v[0]]:
                 if edge not in visited:
-                    s.push((edge, level))
+                    level += 1
+                    s.append((edge, level))
 
     if tree[-1][1] == tree[-2][1]:
-        if tree[-1][1] > tree[-2][1]:
+        if tree[-1][0] > tree[-2][0]:
             return tree[-2][0]
         else:
             return tree[-1][0]
-
-    return tree[-1][0]
+    else:
+        return tree[-1][0]
