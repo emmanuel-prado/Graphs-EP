f598f74f8305261310a801a7de600c224f56e6d1
Emmanium <pradoe.ep@gmail.com>
2020-04-24 11:26:46 -0600

    setup Graph tree, no algorithms runnin yet

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
+# https://docs.python.org/3/library/typing.html
+from typing import List, Dict, Tuple
 
-def earliest_ancestor(ancestors, starting_node):
+AncestryList = List[Tuple(int, int)]
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = Dict[int, int]
+
+    def build_tree(self, dataset: AncestryList) -> Dict:
+        pass
+
+    def add_vertex(self, v: int) -> str:
+        pass
+
+    def add_edge(self, v1: int, v2: int) -> str:
+        pass
+
+    def get_edge(self, vertex: int) -> int:
+        pass
+
+
+def earliest_ancestor(ancestors: AncestryList, starting_node: int):
     pass
